7bb9935eb90cdbc13aeed2b90ae21b56b9bb48e9
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2016-04-28 18:10:58 +0200

    fixed all tests

--- shardis-ui/src/main/frontend/app/app.spec.ts
+++ shardis-ui/src/main/frontend/app/app.spec.ts
@@ -8,11 +8,24 @@ import {
 
 // Load the implementations that should be tested
 import {App} from './app.component';
+import {AuthService} from './services/auth.service';
+import {BaseRequestOptions, Http} from 'angular2/http';
+import {MockBackend} from 'angular2/src/http/backends/mock_backend';
+import {provide} from 'angular2/core';
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
-    App
+    App,
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {
+      useFactory: function (backend, defaultOptions) {
+        return new Http(backend, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    }),
+    AuthService,
   ]);
 
   it('should have a url', inject([App], (app) => {
