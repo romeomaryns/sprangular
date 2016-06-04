73019c054b99e95e17deadb63bdb033cf463c7c5
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2016-06-04 22:06:28 +0200

    preparing login page

--- shardis-ui/src/main/frontend/app/views/login/login.component.ts
+++ shardis-ui/src/main/frontend/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 console.log('`Login` component loaded asynchronously');
 
@@ -9,9 +11,17 @@ console.log('`Login` component loaded asynchronously');
 })
 export class Login implements OnInit {
 
-  constructor() {
+  username = 'admin';
+  password = 'xxxxxx';
+
+  constructor(public authService: AuthService, public router: Router) {
   }
 
+  logMeIn() {
+    console.log('LogMeIn');
+    this.authService.authenticate();
+    this.router.navigate(['/about']);
+  }
 
   ngOnInit():any {
     console.log('hello `Login` component');
