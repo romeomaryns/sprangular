888ddcf1c024347afeb436d0e2b1383124553e31
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2016-06-26 19:44:32 +0200

    fixed crud test

--- shardis-ui/src/main/frontend/app/+crud/crud.component.spec.ts
+++ shardis-ui/src/main/frontend/app/+crud/crud.component.spec.ts
@@ -1,4 +1,7 @@
-import {it, inject, describe, beforeEachProviders} from '@angular/core/testing';
+import {it, inject, beforeEachProviders} from '@angular/core/testing';
+import {BaseRequestOptions, Http} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {AuthService} from '../shared';
 import {Crud} from './crud.component';
 
 // Load the implementations that should be tested
@@ -6,14 +9,24 @@ import {Crud} from './crud.component';
 describe('Crud', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
+    BaseRequestOptions,
+    MockBackend,
+    {
+      provide: Http,
+      useFactory: function (backend, defaultOptions) {
+        return new Http(backend, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    },
+    AuthService,
     Crud
   ]);
 
-  it('should log ngOnInit', inject([Crud], (about) => {
+  it('should log ngOnInit', inject([Crud], (crud) => {
     spyOn(console, 'log');
     expect(console.log).not.toHaveBeenCalled();
 
-    about.ngOnInit();
+    crud.ngOnInit();
     expect(console.log).toHaveBeenCalled();
   }));
 
