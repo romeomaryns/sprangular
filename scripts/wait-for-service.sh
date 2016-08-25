3829c55ed91244e1e3df2eb82ed2ee070873bd13
kucharzyk.tomasz <TomaszKucharzyk@simple.com.pl>
2016-08-25 12:08:23 +0200

    #30 Move wait-for-service.sh script to one place

--- scripts/wait-for-service.sh
+++ scripts/wait-for-service.sh
@@ -5,7 +5,7 @@ SERVICE_URL="$1"
 echo "Waiting for endpoint $1"
 
 while [ -z ${DISCOVERY_SERVICE_READY} ]; do
-  echo "Waiting for discovery service..."
+  echo "Waiting for endpoint $1"
   if [ "$(curl --silent $SERVICE_URL 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       DISCOVERY_SERVICE_READY=true;
   fi
